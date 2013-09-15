e223b3fba506972bbe1871deed2ba8af39c7964b
Lindsey Smith <awkward.lion@gmail.com>
2013-09-14 21:01:26 -0700

    StringifyJSON works!

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -3,5 +3,35 @@
 
 // but you don't so you're going to have to write it from scratch:
 var stringifyJSON = function (obj) {
-  // your code goes here
+    if(obj instanceof Array){
+		var message = "[";
+		for (var i = 0; i < obj.length-1; i++){
+			message += stringifyJSON(obj[i]) + ",";
+		}
+		message += stringifyJSON(obj[obj.length-1]) + "]";
+		return message;
+	} else if(obj === null){
+		return '' + obj;
+	} else if (obj === undefined){
+		return '';
+	} else if (typeof obj === "object"){
+		var result = false;
+		var message = "{";
+		for (var key in obj) {
+			if (typeof obj[key] === 'function'){
+				return "{}";
+			}
+			message += stringifyJSON(key) + ":" + stringifyJSON(obj[key]) + ",";
+			var result = true;
+		}
+		if (result){
+			return message.substring(0, message.length - 1) + "}";
+		} else {
+			return "{}";
+		}
+	} else if (typeof obj === "boolean" || typeof obj === "number"){
+		return '' + obj;
+	} else {
+		return "\"" + obj.toString() + "\"";
+	}
 };
