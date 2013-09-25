f42bd351a2660a11ab64294ec9f48e7c9cf54250
Lindsey Smith <awkward.lion@gmail.com>
2013-09-24 19:51:58 -0700

    Messy, but recursion. Will work on it.

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -5,5 +5,30 @@
 
 // But in stead we're going to implement it from scratch:
 var getElementsByClassName = function (className) {
-  // your code here
+	var arrayElements = [];
+	var results = [];
+	$("*").each(function(){
+		if($(this).attr("class") != undefined){
+			arrayElements.push(this);
+		}
+	})
+
+	function checkElements(nodeList){
+		if (nodeList.length == 0) {
+			return null;
+		} else {
+			var thisClass = $(nodeList[0]).attr("class").split(" ");
+
+			for(var i = 0; i < thisClass.length; i++){
+				if(thisClass[i] == className){
+					results.push($(nodeList[0]).context);
+				}
+			}
+			checkElements(nodeList.slice(1))
+		}
+	}
+
+	checkElements(arrayElements);
+
+	return results;
 };
